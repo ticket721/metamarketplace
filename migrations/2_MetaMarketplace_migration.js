cbf8c4557cbfc97a76769b25a2ee1f41d9ff06ab
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-11-04 14:55:29 +0100

    feat: adapt migration to repo mechanisms

--- migrations/2_MetaMarketplace_migration.js
+++ migrations/2_MetaMarketplace_migration.js
@@ -1,5 +1,15 @@
 const { scripts, ConfigManager } = require('@openzeppelin/cli');
 const { add, push, create } = scripts;
+const config = require('../truffle-config');
+
+const hasArtifact = (name) => {
+    return (config && config.extra_config && config.extra_config.external_modules
+        && config.extra_config.external_modules[name] && config.extra_config.external_modules[name].artifact);
+};
+
+const getArtifact = (name) => {
+    return config.extra_config.external_modules[name].artifact;
+}
 
 async function deploy(options) {
 
@@ -7,9 +17,27 @@ async function deploy(options) {
 
     await push(options);
 
-    const network_id = await web3.eth.net.getId();
+    if (hasArtifact('daiplus') && hasArtifact('ticketforge')) {
+        const network_id = await web3.eth.net.getId();
 
-    await create(Object.assign({ contractAlias: 'MetaMarketplace', methodName: 'initialize_v0', methodArgs: [network_id] }, options));
+        const DaiPlusArtifact = getArtifact('daiplus').DaiPlus;
+        const DaiPlus = new web3.eth.Contract(DaiPlusArtifact.abi, DaiPlusArtifact.networks[network_id].address);
+        const DaiAddress = await DaiPlus.methods.backer().call();
+        const DaiPlusAddress = DaiPlusArtifact.networks[network_id].address;
+
+        const TicketforgeArtifact = getArtifact('ticketforge').TicketForge;
+        const TicketForgeAddress = TicketforgeArtifact.networks[network_id].address;
+
+        console.log(`Dai Address: ${DaiAddress}`);
+        console.log(`Dai+ Address: ${DaiPlusAddress}`);
+        console.log(`T721 Address: ${TicketForgeAddress}`);
+
+        await create(Object.assign({ contractAlias: 'MetaMarketplace', methodName: 'initialize_v0', methodArgs: [network_id, DaiAddress, DaiPlusAddress, TicketForgeAddress] }, options));
+
+    } else {
+        throw new Error('Deployment requires Ticket721 repo setup to inject daiplus & ticketforge configuration');
+    }
+
 }
 
 module.exports = async function(deployer, networkName, accounts) {
